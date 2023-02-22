f04b117df3855d0892753d2e114d30d63dcfecfc
gogolashvili <ggogolashvili91@gmail.com>
2023-02-23 02:46:05 +0400

    select buttons in js

---
+++ main.js
@@ -0,0 +1,17 @@
+let products = [
+  ["Apple", 2.4],
+  ["Banana", 1.8],
+  ["Cherry", 3],
+  ["Peach", 1.2],
+  ["Watermelon", 0.7],
+  ["Melon", 1],
+  ["Plum", 3, 2],
+];
+
+let lowHigh = document.querySelector(".but1");
+let highLow = document.querySelector(".but2");
+let az = document.querySelector(".but3");
+let za = document.querySelector(".but4");
+let min = document.querySelector(".Min");
+let max = document.querySelector(".Max");
+let filter = document.querySelector(".filter");
